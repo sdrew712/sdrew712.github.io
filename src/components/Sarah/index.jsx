1e334e35e0c5bb8871fdf045dc577b45940fffe9
Sarah Drew <drew.sarah.e@gmail.com>
2022-02-05 15:02:33 -0700

    updated Sarah font size on breakpoints

--- src/components/Sarah/index.jsx
+++ src/components/Sarah/index.jsx
@@ -4,14 +4,14 @@ import "./index.css";
 const Sarah = () => {
   return (
     <div id="sarah">
-      <p>Hi, I'm</p>
+      <p className="intro-text">Hi, I'm</p>
       <h1 className="pop-text" id="name">
         Sarah Drew
       </h1>
-      <p>
+      <p className="intro-text">
         a <span className="pop-text">full-stack</span> developer & <span className="pop-text">creator.</span>
       </p>
-      <p>
+      <p className="intro-text">
         I spend my time <span className="pop-text">skating,</span> writing <span className="pop-text">music,</span> and
         hacking on new <span className="pop-text">projects.</span>
       </p>
