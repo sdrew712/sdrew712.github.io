ddf327365d6fbf4b7be147be52179569d898e140
Sarah Drew <drew.sarah.e@gmail.com>
2022-02-05 14:24:12 -0700

    added react-headroom

--- src/components/TopNav/index.jsx
+++ src/components/TopNav/index.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import Headroom from "react-headroom";
 import "./index.css";
 
 const TopNav = () => {
   return (
-    <div id="top-nav">
-      <a href="#sarah">Sarah</a>
+    <Headroom>
+      <div id="top-nav">
+        <a href="#sarah">Sarah</a>
 
-      <div>
-        <a href="#projects" className="nav-item-right">
-          Projects
-        </a>
-        <a href="#journey" className="nav-item-right">
-          Journey
-        </a>
-        <a href="#contact" className="nav-item-right">
-          Contact
-        </a>
+        <div>
+          <a href="#projects" className="nav-item-right">
+            Projects
+          </a>
+          <a href="#journey" className="nav-item-right">
+            Journey
+          </a>
+          <a href="#contact" className="nav-item-right">
+            Contact
+          </a>
+        </div>
       </div>
-    </div>
+    </Headroom>
   );
 };
 
