df3383505e62cc2f9538916872a804eec429161e
Sarah Drew <drew.sarah.e@gmail.com>
2022-02-06 11:22:56 -0700

    extracted padding at media queries to section selector

--- src/components/Sarah/index.jsx
+++ src/components/Sarah/index.jsx
@@ -3,19 +3,21 @@ import "./index.css";
 
 const Sarah = () => {
   return (
-    <div id="sarah">
+    <section id="sarah">
       <p className="intro-text">Hi, I'm</p>
       <h1 className="pop-text" id="name">
         Sarah Drew
       </h1>
       <p className="intro-text">
-        a <span className="pop-text">full-stack</span> developer & <span className="pop-text">creator.</span>
+        a <span className="pop-text">full-stack</span> developer &{" "}
+        <span className="pop-text">creator.</span>
       </p>
       <p className="intro-text">
-        I spend my time <span className="pop-text">skating,</span> writing <span className="pop-text">music,</span> and
-        hacking on new <span className="pop-text">projects.</span>
+        I spend my time <span className="pop-text">skating,</span> writing{" "}
+        <span className="pop-text">music,</span> and hacking on new{" "}
+        <span className="pop-text">projects.</span>
       </p>
-    </div>
+    </section>
   );
 };
 
