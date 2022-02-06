5d41f77415bf411955f09cb29493ff2ccbb87b90
Sarah Drew <drew.sarah.e@gmail.com>
2022-02-05 23:18:01 -0700

    added Projects section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import TopNav from "./components/TopNav";
 import Sarah from "./components/Sarah";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <TopNav />
       <Sarah />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header";
+import "./index.css";
+
+const Projects = () => {
+  return (
+    <>
+      <Header title="Projects" />
+    </>
+  );
+};
+
+export default Projects;
