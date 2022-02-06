b12b7756d502a01ade5cf4ddeb34050f599d4a19
Sarah Drew <drew.sarah.e@gmail.com>
2022-02-06 03:09:22 -0700

    change img of project to div background image

--- src/components/Projects/ProjectCard/index.jsx
+++ src/components/Projects/ProjectCard/index.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import "./index.css";
 
 const ProjectCard = ({ image, alt, title, description, tags }) => {
+  const divImage = {
+    backgroundImage: "url(" + image + ")",
+  };
+
   return (
-    <div>
-      <img src={image} alt={alt} />
+    <div id="project-card" style={divImage}>
       <h3>{title}</h3>
       <p>{description}</p>
-      {tags.map((tag) => (
-        <span className="tag">{tag}</span>
+      {tags.map((tag, index) => (
+        <span key={index} className="tag">
+          {tag}
+        </span>
       ))}
       <div>View on GitHub</div>
     </div>
