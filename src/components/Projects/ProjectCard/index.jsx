ad092daa14daeb207a74d4b9b48d1165a4deef70
Sarah Drew <drew.sarah.e@gmail.com>
2022-02-06 00:00:33 -0700

    added props to ProjectCard

--- src/components/Projects/ProjectCard/index.jsx
+++ src/components/Projects/ProjectCard/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./index.css";
 
-const ProjectCard = () => {
-  return <div></div>;
+const ProjectCard = ({ image, alt, title, description, tags }) => {
+  return (
+    <div>
+      <img src={image} alt={alt} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      {tags.map((tag) => (
+        <span className="tag">{tag}</span>
+      ))}
+      <div>View on GitHub</div>
+    </div>
+  );
 };
 
 export default ProjectCard;
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Header from "../Header";
+import ProjectCard from "./ProjectCard";
 import "./index.css";
 
 const Projects = () => {
   return (
     <>
       <Header title="Projects" />
+      <div id="projects">
+        <ProjectCard
+          image="images/dust-eater.jpg"
+          alt="Completed robot car"
+          title="Dust Eater"
+          description="My Particle Photon powered IoT robot car"
+          tags={["React", "Arduino", "Particle Photon"]}
+        />
+        <ProjectCard
+          image="images/board-hoard.png"
+          alt="Front page of Board Hoard website"
+          title="Board Hoard"
+          description="A longboard e-commerce shop."
+          tags={["React", "Node.js", "Express", "Prisma", "Stripe"]}
+        />
+        <ProjectCard
+          image="images/rate-noods.png"
+          alt="Front page of Rate Noods website"
+          title="Rate Noods"
+          description="The start of my coding journey, a many-times-reworked instant noodle rating website!"
+          tags={["React", "Amazon S3"]}
+        />
+      </div>
     </>
   );
 };
