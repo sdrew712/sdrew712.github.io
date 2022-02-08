bb609c0f16c05d331596881782f518a17b7cead2
Sarah Drew <drew.sarah.e@gmail.com>
2022-02-07 19:18:05 -0700

    added html for JourneyItem

--- src/components/Journey/JourneyItem/index.jsx
+++ src/components/Journey/JourneyItem/index.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./index.css";
 
-const JourneyItem = () => {
-  return <div></div>;
+const JourneyItem = ({ title, location, dates, details }) => {
+  return (
+    <div>
+      <div>
+        <h2>{title}</h2>
+        <h2 className="pop-text">{location}</h2>
+      </div>
+      <div>{dates}</div>
+      {details.map((detail, index) => (
+        <div>
+          <span>▹</span>
+          <p key={index}>{detail}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default JourneyItem;
