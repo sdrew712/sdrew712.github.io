ea10e2154172ca4f2fac037e55aa0e9da6a434d7
Sarah Drew <drew.sarah.e@gmail.com>
2022-02-21 22:55:01 -0700

    added active nav item effect!

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import VisibilitySensor from "react-visibility-sensor";
 import NavBar from "./components/NavBar";
 import Sarah from "./components/Sarah";
 import Projects from "./components/Projects";
@@ -6,13 +8,61 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
+  const [active, setActive] = useState("sarah");
+
+  function changeVisible(isVisible, section) {
+    if (isVisible) {
+      setActive(section);
+    }
+    // handle case for switch sections when scrolling up
+    // because the sensor component either detects elements
+    // completely in view, or based off offset from top of elements.
+    // this handles switching 'active' sections when scrolling up into
+    // sections that stretch past the screen height (specifically on Mobile)
+    else if (!isVisible) {
+      switch (section) {
+        case "projects":
+          if (active === "projects") setActive("sarah");
+          break;
+        case "journey":
+          if (active === "journey") setActive("projects");
+          break;
+        case "contact":
+          if (active === "contact") setActive("journey");
+          break;
+        default:
+      }
+    }
+  }
+
   return (
     <>
-      <NavBar />
-      <Sarah />
-      <Projects />
-      <Journey />
-      <Contact />
+      <NavBar active={active} />
+
+      <VisibilitySensor
+        onChange={(isVisible) => changeVisible(isVisible, "sarah")}
+      >
+        <Sarah />
+      </VisibilitySensor>
+
+      <VisibilitySensor
+        onChange={(isVisible) => changeVisible(isVisible, "projects")}
+      >
+        <Projects />
+      </VisibilitySensor>
+
+      <VisibilitySensor
+        onChange={(isVisible) => changeVisible(isVisible, "journey")}
+      >
+        <Journey />
+      </VisibilitySensor>
+
+      <VisibilitySensor
+        onChange={(isVisible) => changeVisible(isVisible, "contact")}
+      >
+        <Contact />
+      </VisibilitySensor>
+
       <Footer />
     </>
   );
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,20 +1,48 @@
+import { useEffect } from "react";
 import Headroom from "react-headroom";
 import "./index.css";
 
-const NavBar = () => {
+const NavBar = ({ active }) => {
+  useEffect(() => {
+    const activeNavItem = document.getElementById("active");
+    const navItemName = activeNavItem.name;
+    activeNavItem.innerText = `<${activeNavItem.innerText} />`;
+    return () => {
+      activeNavItem.innerText =
+        navItemName[0].toUpperCase() + navItemName.slice(1);
+    };
+  }, [active]);
+
   return (
     <Headroom>
       <nav id="nav-bar">
-        <a href="#sarah">Sarah</a>
+        <a href="#sarah" name="sarah" id={active === "sarah" ? "active" : ""}>
+          Sarah
+        </a>
 
         <div>
-          <a href="#projects" className="nav-item-right">
+          <a
+            href="#projects"
+            className="nav-item-right"
+            name="projects"
+            id={active === "projects" ? "active" : ""}
+          >
             Projects
           </a>
-          <a href="#journey" className="nav-item-right">
+          <a
+            href="#journey"
+            className="nav-item-right"
+            name="journey"
+            id={active === "journey" ? "active" : ""}
+          >
             Journey
           </a>
-          <a href="#contact" className="nav-item-right">
+          <a
+            href="#contact"
+            className="nav-item-right"
+            name="contact"
+            id={active === "contact" ? "active" : ""}
+          >
             Contact
           </a>
         </div>
