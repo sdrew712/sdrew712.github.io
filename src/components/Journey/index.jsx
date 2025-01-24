f6f261d3b2ed517f807c5234b8a6b95771676fd3
Sarah Drew <drew.sarah.e@gmail.com>
2025-01-24 01:42:42 -0700

    update journey

--- src/components/Journey/index.jsx
+++ src/components/Journey/index.jsx
@@ -8,31 +8,13 @@ const Journey = () => {
       <Header title="Journey" />
       <div id="journey-section">
         <JourneyItem
-          title="Engineer"
+          title="Software Engineer"
           location="@ Sibi"
-          dates="Nov 2021 - Present"
+          dates="Nov 2021 - Now"
         >
           <p>
-            Facilitate connections between manufacturers and clients through the
-            Sibi platform
-          </p>
-          <p>
-            Collaborate with team to develop
-            <span className="pop-text"> authorization</span> and
-            <span className="pop-text"> authentication </span>
-            infrastructure
-          </p>
-          <p>
-            Write and review code for
-            <span className="pop-text"> Next JS </span>platform
-          </p>
-          <p>
-            Manage database using <span className="pop-text">Prisma</span>
-          </p>
-          <p>
-            Utilize <span className="pop-text">Cypress </span> and
-            <span className="pop-text"> Jest </span> to automate testing in
-            <span className="pop-text"> CI/CD </span> process
+            Engineering products for our clients and manufacturers in the real
+            estate sector
           </p>
         </JourneyItem>
 
@@ -41,18 +23,7 @@ const Journey = () => {
           location="@ DevMountain"
           dates="July 2021 - Nov 2021"
         >
-          <p>Pair programmed daily to complete coding exercises and projects</p>
-          <p>
-            Developed full stack applications using
-            <span className="pop-text"> Express </span> as a backend,
-            <span className="pop-text"> React </span>as a frontend, and
-            <span className="pop-text"> PostgreSQL </span>as a database
-          </p>
-          <p>
-            Built final project, an ecommerce website, with
-            <span className="pop-text"> React </span> and the{" "}
-            <span className="pop-text"> Stripe API</span>
-          </p>
+          <p>Software development bootcamp</p>
         </JourneyItem>
       </div>
     </section>
